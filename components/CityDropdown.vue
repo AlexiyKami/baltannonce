bb383bdce5afab004bebec29a0f0173bfee1facd
Alex <AlexiyKami@github.com>
2024-06-05 12:44:32 +0300

    feat: hero-section

---
+++ components/CityDropdown.vue
@@ -0,0 +1,95 @@
+<script setup>
+import '../assets/scss/components/city-dropdown.scss';
+
+const isCityMenuOpened = ref(true);
+</script>
+
+<template>
+  <div class="hero__dropdown-city dropdown-city">
+    <div class="dropdown-city__btn" @click="isCityMenuOpened = !isCityMenuOpened">
+      <span class="dropdown-city__text button__text">Выберите город</span>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="dropdown-city__chevron"
+        :class="{active: isCityMenuOpened}"
+      >
+        <path
+          d="M6 9L12 15L18 9"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+    <div class="dropdown-city__body" :class="{active: isCityMenuOpened}">
+      <div class="dropdown-city__container lt">
+        <span class="dropdown-city__text">Литва</span>
+        <ul class="dropdown-city__list">
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+        </ul>
+      </div>
+      <div class="dropdown-city__container lv">
+        <span class="dropdown-city__text">Латвия</span>
+        <ul class="dropdown-city__list">
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+        </ul>
+      </div>
+      <div class="dropdown-city__container ee">
+        <span class="dropdown-city__text">Эстония</span>
+        <ul class="dropdown-city__list">
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+          <li class="dropdown-city__item">
+          <span class="dropdown-city__city-name">Вильнюс</span>
+          <span class="dropdown-city__city-count">(0)</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,11 @@ import '../assets/scss/widgets/hero-section.scss';
 
 <template>
   <main>
-    Main
+    <section class="hero">
+      <div class="hero__container _container">
+        <h1 class="hero__title heading_h1">TEST</h1>
+        <CityDropdown></CityDropdown>
+      </div>
+    </section>
   </main>
 </template>
