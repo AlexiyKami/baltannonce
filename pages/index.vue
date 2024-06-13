b777e0d94f18cdbecc5142b91943c533f409491a
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-13 21:15:42 +0200

    feat: add pages folder

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { HeroSection } from '@/widgets/HeroSection';
+import { AdvertisementSection } from '@/widgets/AdvertisementSection';
+import { HomeSection } from '@/widgets/HomeSection';
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <div>
+        <HeroSection />
+        <AdvertisementSection />
+        <HomeSection />
+      </div>
+      <template #fallback>Loading...</template>
+    </Suspense>
+  </div>
+</template>
---
+++ pages/questionary.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Questionary</div>
+</template>
