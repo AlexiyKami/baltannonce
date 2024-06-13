143d9b1bd8be030ede7416e2714942e13949c4dc
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-13 21:10:10 +0200

    init: start nuxt project

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+useSeoMeta({
+  title: 'Baltannonce',
+  ogTitle: 'Baltannonce'
+});
+</script>
+
+<template>
+  <div class="wrapper">
+    <NuxtLayout>
+      <NuxtErrorBoundary>
+        <div class="main">
+          <NuxtPage />
+        </div>
+        <template #error="{ error }">
+          <p>An error occurred: {{ error }}</p>
+        </template>
+      </NuxtErrorBoundary>
+    </NuxtLayout>
+  </div>
+</template>
--- girl.js
+++
@@ -1,4 +0,0 @@
-import './components/city-dropdown.js'
-import './components/dropdown.js'
-import './components/filter-dropdown.js'
-import './components/extended-filter-modal.js'
