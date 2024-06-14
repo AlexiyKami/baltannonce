0d3dc2d574082cc7313d3a2a555c7b613a064ce2
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-14 15:11:16 +0200

    feat: add cities object for city dropdown

---
+++ entities/Girls/model/cities.js
@@ -0,0 +1,56 @@
+export const lithuanianCities = [
+  {
+    title: 'Вильнюс',
+    count: 0
+  },
+  {
+    title: 'Каунас',
+    count: 0
+  },
+  {
+    title: 'Клайпеда',
+    count: 0
+  },
+  {
+    title: 'Шяуляй',
+    count: 0
+  },
+  {
+    title: 'Паневежис',
+    count: 0
+  }
+];
+
+export const latvianCities = [
+  {
+    title: 'Рига',
+    count: 0
+  },
+  {
+    title: 'Юрмала',
+    count: 0
+  },
+  {
+    title: 'Даугавпилс',
+    count: 0
+  },
+  {
+    title: 'Лиепая',
+    count: 0
+  }
+];
+
+export const estonianCities = [
+  {
+    title: 'Таллин',
+    count: 0
+  },
+  {
+    title: 'Тарту',
+    count: 0
+  },
+  {
+    title: 'Нарва',
+    count: 0
+  }
+];
