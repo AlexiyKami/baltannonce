0497bed8d7549fcae9f03f8b06d437835c999316
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-14 13:04:48 +0200

    feat: use girl card in home section

--- entities/Girls/index.js
+++ entities/Girls/index.js
@@ -2,8 +2,10 @@ import CityDropdown from './ui/CityDropdown/CityDropdown.vue';
 import ExtendedFilterModal from './ui/ExtendedFilterModal/ExtendedFilterModal.vue';
 import FilterDropdown from './ui/FilterDropdown/FilterDropdown.vue';
 import HomeAdvertisements from './ui/HomeAdvertisements/HomeAdvertisements.vue';
+import GirlCard from './ui/GirlCard/GirlCard.vue';
 
 export { CityDropdown };
 export { ExtendedFilterModal };
 export { FilterDropdown };
 export { HomeAdvertisements };
+export { GirlCard };
