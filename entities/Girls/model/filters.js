b2fef2c0ab26749ae3dd0c633a4c1ee5937ec0ba
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-14 16:06:40 +0200

    feat: reuse dropdown filter fieldset

---
+++ entities/Girls/model/filters.js
@@ -0,0 +1,159 @@
+export const sortOptions = [
+  {
+    text: 'Новые первые',
+    id: 'new-first'
+  },
+  {
+    text: 'Дешевые первые',
+    id: 'cheap-first'
+  },
+  {
+    text: 'Активности',
+    id: 'activity'
+  },
+  {
+    text: 'Рейтингу',
+    id: 'rating'
+  }
+];
+
+export const priceOptions = [
+  {
+    text: 'От 100 до 200',
+    id: '100-200'
+  },
+  {
+    text: 'От 200 до 300',
+    id: '200-300'
+  },
+  {
+    text: 'От 300 до 500',
+    id: '300-500'
+  }
+];
+
+export const ageOptions = [
+  {
+    text: '18-20',
+    id: '18-20'
+  },
+  {
+    text: '21-24',
+    id: '21-24'
+  },
+  {
+    text: '25-29',
+    id: '25-29'
+  },
+  {
+    text: '30-35',
+    id: '30-35'
+  }
+];
+
+export const hairColorOptions = [
+  {
+    text: 'Блондинка',
+    id: 'blonde'
+  },
+  {
+    text: 'Шатенка',
+    id: 'brown'
+  },
+  {
+    text: 'Брюнетка',
+    id: 'brunette'
+  },
+  {
+    text: 'Русая',
+    id: 'light-brown'
+  },
+  {
+    text: 'Рыжая',
+    id: 'red'
+  }
+];
+
+export const breastSizeOptions = [
+  {
+    text: 'A',
+    id: 'A'
+  },
+  {
+    text: 'B',
+    id: 'B'
+  },
+  {
+    text: 'C',
+    id: 'C'
+  },
+  {
+    text: 'D',
+    id: 'D'
+  }
+];
+
+export const languageOptions = [
+  {
+    text: 'Литовский',
+    id: 'lt'
+  },
+  {
+    text: 'Латышский',
+    id: 'lv'
+  },
+  {
+    text: 'Эстонский',
+    id: 'ee'
+  },
+  {
+    text: 'Русский',
+    id: 'ru'
+  },
+  {
+    text: 'Английский',
+    id: 'en'
+  }
+];
+
+export const raceOptions = [
+  {
+    text: 'Белая',
+    id: 'white'
+  },
+  {
+    text: 'Латинская',
+    id: 'latin'
+  },
+  {
+    text: 'Черная',
+    id: 'negro'
+  },
+  {
+    text: 'Азиатка',
+    id: 'asian'
+  },
+  {
+    text: 'Экзотическая',
+    id: 'exotic'
+  }
+];
+
+export const availableOptions = [
+  {
+    text: 'Апартаменты Incall',
+    id: 'incall-a'
+  },
+  {
+    text: 'Отель Incall',
+    id: 'incall-h'
+  },
+  {
+    text: 'Апартаменты Outcall',
+    id: 'outcall-a'
+  },
+  {
+    text: 'Отель Outcall',
+    id: 'outcall-h'
+  }
+];
---
+++ shared/ui/DropdownFilterFieldset/DropdownFilterFieldset.vue
@@ -0,0 +1,16 @@
+<script setup>
+import './dropdown-filter-fieldset.scss';
+
+defineProps(['title', 'type', 'name', 'options']);
+</script>
+
+<template>
+  <div class="dropdown-filter__fieldset dropdown-filter__sortby">
+    <h3 class="dropdown-filter__title heading_h5">{{ title }}</h3>
+    <div class="dropdown-filter__item" v-for="option in options" :key="option">
+      <input :id="option.id" :type="type" :name="name" />
+      <label :for="option.id">{{ option.text }}</label>
+    </div>
+    <slot />
+  </div>
+</template>
