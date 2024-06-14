3ba6f402779e0db77e556ff99d711e358a2e49bc
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-14 13:01:51 +0200

    refactor: replace home advertisements component to another folder

--- entities/Girls/index.js
+++ entities/Girls/index.js
@@ -1,7 +1,9 @@
 import CityDropdown from './ui/CityDropdown/CityDropdown.vue';
 import ExtendedFilterModal from './ui/ExtendedFilterModal/ExtendedFilterModal.vue';
 import FilterDropdown from './ui/FilterDropdown/FilterDropdown.vue';
+import HomeAdvertisements from './ui/HomeAdvertisements/HomeAdvertisements.vue';
 
 export { CityDropdown };
 export { ExtendedFilterModal };
 export { FilterDropdown };
+export { HomeAdvertisements };
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { HeroSection } from '@/widgets/HeroSection';
-import { AdvertisementSection } from '@/widgets/AdvertisementSection';
 import { HomeSection } from '@/widgets/HomeSection';
 </script>
 
 <template>
   <div>
     <HeroSection />
-    <AdvertisementSection />
     <HomeSection />
   </div>
 </template>
