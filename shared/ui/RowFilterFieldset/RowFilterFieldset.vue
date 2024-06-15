74d8bb1ba0814d748531947dac641078a0a6b3ef
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-15 18:47:15 +0200

    feat: add row filter fieldset component

---
+++ shared/ui/RowFilterFieldset/RowFilterFieldset.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['title', 'options', 'type', 'name']);
+</script>
+
+<template>
+  <div class="item-extended-filter__appearance">
+    <h6 class="heading_h6 heading__align-self_start">{{ title }}</h6>
+    <div
+      class="item-extended-filter__row wrap"
+      :class="{
+        'item-extended-filter__languages': name === 'languages'
+      }"
+    >
+      <div class="item-extended-filter__row" v-for="option in options" :key="option.id">
+        <input :id="option.id" :type="type" :name="name" />
+        <label :for="option.id">{{ option.title }}</label>
+      </div>
+    </div>
+  </div>
+</template>
--- shared/ui/index.js
+++ shared/ui/index.js
@@ -1,7 +1,9 @@
 import PopupDropdownFilter from './PopupDropdownFilter/PopupDropdownFilter.vue';
 import DropdownItemWithCount from './DropdownItemWithCount/DropdownItemWithCount.vue';
 import FilterFieldset from './FilterFieldset/FilterFieldset.vue';
+import RowFilterFieldset from './RowFilterFieldset/RowFilterFieldset.vue';
 
 export { PopupDropdownFilter };
 export { DropdownItemWithCount };
 export { FilterFieldset };
+export { RowFilterFieldset };
