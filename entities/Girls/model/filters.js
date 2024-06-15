b88caa9cc06d3e1f1d6fa0233bffe6251a188420
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-15 18:42:57 +0200

    refactor: change filters object

--- entities/Girls/model/filters.js
+++ entities/Girls/model/filters.js
@@ -1,225 +1,214 @@
-export const sortOptions = [
-  {
-    text: 'Новые первые',
-    id: 'new-first'
-  },
-  {
-    text: 'Дешевые первые',
-    id: 'cheap-first'
-  },
-  {
-    text: 'Активности',
-    id: 'activity'
-  },
-  {
-    text: 'Рейтингу',
-    id: 'rating'
-  }
-];
-
-export const priceOptions = [
-  {
-    text: 'От 100 до 200',
-    id: '100-200'
-  },
-  {
-    text: 'От 200 до 300',
-    id: '200-300'
-  },
-  {
-    text: 'От 300 до 500',
-    id: '300-500'
-  }
-];
-
-export const ageOptions = [
-  {
-    text: '18-20',
-    id: '18-20'
-  },
-  {
-    text: '21-24',
-    id: '21-24'
-  },
-  {
-    text: '25-29',
-    id: '25-29'
-  },
-  {
-    text: '30-35',
-    id: '30-35'
-  }
-];
-
-export const hairColorOptions = [
-  {
-    text: 'Блондинка',
-    id: 'blonde'
-  },
-  {
-    text: 'Шатенка',
-    id: 'brown'
-  },
-  {
-    text: 'Брюнетка',
-    id: 'brunette'
-  },
-  {
-    text: 'Русая',
-    id: 'light-brown'
-  },
-  {
-    text: 'Рыжая',
-    id: 'red'
-  }
-];
-
-export const breastSizeOptions = [
-  {
-    text: 'A',
-    id: 'A'
-  },
-  {
-    text: 'B',
-    id: 'B'
-  },
-  {
-    text: 'C',
-    id: 'C'
-  },
-  {
-    text: 'D',
-    id: 'D'
-  }
-];
-
-export const languageOptions = [
-  {
-    text: 'Литовский',
-    id: 'lt'
-  },
-  {
-    text: 'Латышский',
-    id: 'lv'
-  },
-  {
-    text: 'Эстонский',
-    id: 'ee'
-  },
-  {
-    text: 'Русский',
-    id: 'ru'
-  },
-  {
-    text: 'Английский',
-    id: 'en'
-  }
-];
-
-export const raceOptions = [
-  {
-    text: 'Белая',
-    id: 'white'
-  },
-  {
-    text: 'Латинская',
-    id: 'latin'
-  },
-  {
-    text: 'Черная',
-    id: 'negro'
-  },
-  {
-    text: 'Азиатка',
-    id: 'asian'
-  },
-  {
-    text: 'Экзотическая',
-    id: 'exotic'
-  }
-];
-
-export const availableOptions = [
-  {
-    text: 'Апартаменты Incall',
-    id: 'incall-a'
-  },
-  {
-    text: 'Отель Incall',
-    id: 'incall-h'
-  },
-  {
-    text: 'Апартаменты Outcall',
-    id: 'outcall-a'
-  },
-  {
-    text: 'Отель Outcall',
-    id: 'outcall-h'
-  }
-];
-
-export const questionaryOptions = [
-  {
-    text: 'Да',
-    id: 'questionary-yes'
-  },
-  {
-    text: 'Нет',
-    id: 'questionary-no'
-  }
-];
-
-export const smokingOptions = [
-  {
-    text: 'Да',
-    id: 'smoking-yes'
-  },
-  {
-    text: 'Нет',
-    id: 'smoking-no'
-  }
-];
-
-export const siliconeOptions = [
-  {
-    text: 'Да',
-    id: 'silicone-yes'
-  },
-  {
-    text: 'Нет',
-    id: 'silicone-no'
-  }
-];
-
-export const bdsmOptions = [
-  {
-    text: 'Да',
-    id: 'bdsm-yes'
-  },
-  {
-    text: 'Нет',
-    id: 'bdsm-no'
-  }
-];
-
-export const contactsOptions = [
-  {
-    text: 'Telegram',
-    id: 'telegram'
-  },
-  {
-    text: 'Whatsapp',
-    id: 'whatsapp'
-  }
-];
-
-export const verifiedOptions = [
-  {
-    text: 'Да',
-    id: 'verified-yes'
-  },
-  {
-    text: 'Нет',
-    id: 'verified-no'
-  }
-];
+export const filters = {
+  sortOptions: [
+    {
+      title: 'Новые первые',
+      id: 'new-first'
+    },
+    {
+      title: 'Дешевые первые',
+      id: 'cheap-first'
+    },
+    {
+      title: 'Активности',
+      id: 'activity'
+    },
+    {
+      title: 'Рейтингу',
+      id: 'rating'
+    }
+  ],
+  priceOptions: [
+    {
+      title: 'От 100 до 200',
+      id: '100-200'
+    },
+    {
+      title: 'От 200 до 300',
+      id: '200-300'
+    },
+    {
+      title: 'От 300 до 500',
+      id: '300-500'
+    }
+  ],
+  ageOptions: [
+    {
+      title: '18-20',
+      id: '18-20'
+    },
+    {
+      title: '21-24',
+      id: '21-24'
+    },
+    {
+      title: '25-29',
+      id: '25-29'
+    },
+    {
+      title: '30-35',
+      id: '30-35'
+    }
+  ],
+  hairColorOptions: [
+    {
+      title: 'Блондинка',
+      id: 'blonde'
+    },
+    {
+      title: 'Шатенка',
+      id: 'brown'
+    },
+    {
+      title: 'Брюнетка',
+      id: 'brunette'
+    },
+    {
+      title: 'Русая',
+      id: 'light-brown'
+    },
+    {
+      title: 'Рыжая',
+      id: 'red'
+    }
+  ],
+  breastSizeOptions: [
+    {
+      title: 'A',
+      id: 'A'
+    },
+    {
+      title: 'B',
+      id: 'B'
+    },
+    {
+      title: 'C',
+      id: 'C'
+    },
+    {
+      title: 'D',
+      id: 'D'
+    }
+  ],
+  languageOptions: [
+    {
+      title: 'Литовский',
+      id: 'lt'
+    },
+    {
+      title: 'Латышский',
+      id: 'lv'
+    },
+    {
+      title: 'Эстонский',
+      id: 'ee'
+    },
+    {
+      title: 'Русский',
+      id: 'ru'
+    },
+    {
+      title: 'Английский',
+      id: 'en'
+    }
+  ],
+  raceOptions: [
+    {
+      title: 'Белая',
+      id: 'white'
+    },
+    {
+      title: 'Латинская',
+      id: 'latin'
+    },
+    {
+      title: 'Черная',
+      id: 'negro'
+    },
+    {
+      title: 'Азиатка',
+      id: 'asian'
+    },
+    {
+      title: 'Экзотическая',
+      id: 'exotic'
+    }
+  ],
+  availableOptions: [
+    {
+      title: 'Апартаменты Incall',
+      id: 'incall-a'
+    },
+    {
+      title: 'Отель Incall',
+      id: 'incall-h'
+    },
+    {
+      title: 'Апартаменты Outcall',
+      id: 'outcall-a'
+    },
+    {
+      title: 'Отель Outcall',
+      id: 'outcall-h'
+    }
+  ],
+  questionaryOptions: [
+    {
+      title: 'Да',
+      id: 'questionary-yes'
+    },
+    {
+      title: 'Нет',
+      id: 'questionary-no'
+    }
+  ],
+  smokingOptions: [
+    {
+      title: 'Да',
+      id: 'smoking-yes'
+    },
+    {
+      title: 'Нет',
+      id: 'smoking-no'
+    }
+  ],
+  siliconeOptions: [
+    {
+      title: 'Да',
+      id: 'silicone-yes'
+    },
+    {
+      title: 'Нет',
+      id: 'silicone-no'
+    }
+  ],
+  bdsmOptions: [
+    {
+      title: 'Да',
+      id: 'bdsm-yes'
+    },
+    {
+      title: 'Нет',
+      id: 'bdsm-no'
+    }
+  ],
+  contactsOptions: [
+    {
+      title: 'Telegram',
+      id: 'telegram'
+    },
+    {
+      title: 'Whatsapp',
+      id: 'whatsapp'
+    }
+  ],
+  verifiedOptions: [
+    {
+      title: 'Да',
+      id: 'verified-yes'
+    },
+    {
+      title: 'Нет',
+      id: 'verified-no'
+    }
+  ]
+};
