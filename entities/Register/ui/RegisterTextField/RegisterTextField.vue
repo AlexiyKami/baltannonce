35c7d121db366a68e560cc0c97e37a77601de36b
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-16 15:57:18 +0200

    feat: create register text field component

---
+++ entities/Register/ui/RegisterTextField/RegisterTextField.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  placeholder: { type: String, required: true },
+  type: { type: String, required: true },
+  value: { type: String, required: true }
+});
+
+const emit = defineEmits(['setValue']);
+
+const handleInput = (e) => {
+  emit('setValue', e.target.value);
+};
+</script>
+
+<template>
+  <input
+    class="register__input register__input-email"
+    :type="type"
+    :value="value"
+    :placeholder="placeholder"
+    required
+    @input="handleInput"
+  >
+</template>
