d14916f87110415756008154b9f0dedad20b2da6
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-16 11:28:26 +0200

    feat: create range input component

---
+++ shared/ui/RangeInput/RangeInput.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps(
+  {
+    name: String
+  },
+  {
+    title: String
+  },
+  { container: String }
+);
+</script>
+
+<template>
+  <div
+    class="item-extended-filter__form wrap"
+    :class="{
+      'item-extended-filter__form  wrap': !container,
+      'item-extended-filter__row': container === 'row'
+    }"
+  >
+    <label class="heading_h6" for="">{{ title }}:</label>
+    <div class="item-extended-filter__row">
+      <input :id="name" type="number" :name="name" class="medium" min="0" placeholder="----" >
+      <span>-</span>
+      <input :id="name" type="number" :name="name" class="medium" min="0" placeholder="----" >
+      <span class="item-extended-filter__unit"><slot /></span>
+    </div>
+  </div>
+</template>
