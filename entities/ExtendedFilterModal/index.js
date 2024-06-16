a3de6c86c74e7f79c55bba49de8611099e377077
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-16 11:33:36 +0200

    feat: create location filter component

---
+++ entities/ExtendedFilterModal/index.js
@@ -0,0 +1,15 @@
+import MainParameters from './ui/MainParameters/MainParameters.vue';
+import PreferencesFilter from './ui/PreferencesFilter/PreferencesFilter.vue';
+import ReviewsRatingsFilter from './ui/ReviewsRatingsFilter/ReviewsRatingsFilter.vue';
+import AdditionalParameters from './ui/AdditionalParameters/AdditionalParameters.vue';
+import AppearanceFilter from './ui/AppearanceFilter/AppearanceFilter.vue';
+import FetishismFilter from './ui/FetishismFilter/FetishismFilter.vue';
+import LocationFilter from './ui/LocationFilter/LocationFilter.vue';
+
+export { MainParameters };
+export { PreferencesFilter };
+export { ReviewsRatingsFilter };
+export { AdditionalParameters };
+export { AppearanceFilter };
+export { FetishismFilter };
+export { LocationFilter };
