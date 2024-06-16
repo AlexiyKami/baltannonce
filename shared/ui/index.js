1e27481955089af7097b52602e8d689433579cb2
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-16 11:29:55 +0200

    refactor: replace cities object to shared folder

--- shared/ui/index.js
+++ shared/ui/index.js
@@ -3,9 +3,13 @@ import DropdownItemWithCount from './DropdownItemWithCount/DropdownItemWithCount
 import FilterFieldset from './FilterFieldset/FilterFieldset.vue';
 import RowFilterFieldset from './RowFilterFieldset/RowFilterFieldset.vue';
 import BaseDropdown from './BaseDropdown/BaseDropdown.vue';
+import RangeInput from './RangeInput/RangeInput.vue';
+import RangeInputSmall from './RangeInputSmall/RangeInputSmall.vue';
 
 export { PopupDropdownFilter };
 export { DropdownItemWithCount };
 export { FilterFieldset };
 export { RowFilterFieldset };
 export { BaseDropdown };
+export { RangeInput };
+export { RangeInputSmall };
