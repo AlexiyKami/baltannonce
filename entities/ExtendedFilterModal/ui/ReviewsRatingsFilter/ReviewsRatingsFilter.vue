afb432a488e75aeb15c95bf843178782798d3c26
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-16 11:32:19 +0200

    feat: add reviews and rating filter component

---
+++ entities/ExtendedFilterModal/ui/ReviewsRatingsFilter/ReviewsRatingsFilter.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { BaseDropdown } from '@/shared/ui';
+</script>
+
+<template>
+  <div class="extended-filter__container extended-filter__container-reviews">
+    <div class="extended-filter__item item-extended-filter">
+      <h4 class="item-extended-filter__title heading_h4">Отзывы и оценки</h4>
+      <div class="item-extended-filter__stripe" />
+      <div class="item-extended-filter__forms-container">
+        <div class="item-extended-filter__forms">
+          <div class="item-extended-filter__reviews">
+            <label class="heading_h6" for="">Отзывов, не менее:</label>
+            <BaseDropdown />
+          </div>
+          <div class="item-extended-filter__reviews">
+            <label class="heading_h6" for="">С оценками пользователей:</label>
+            <div class="button button_secondary button_small">Выбрать</div>
+          </div>
+        </div>
+        <div class="item-extended-filter__forms">
+          <div class="item-extended-filter__reviews">
+            <label class="heading_h6" for="">Оценок, не менее:</label>
+            <BaseDropdown />
+          </div>
+          <div class="item-extended-filter__reviews">
+            <label class="heading_h6" for="">С новыми отзывами за последние:</label>
+            <BaseDropdown />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
