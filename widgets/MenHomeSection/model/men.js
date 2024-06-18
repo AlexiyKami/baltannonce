b8a93e7aaf3f980dd62f554919ba9900adf2574e
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-18 16:18:13 +0200

    feat: add men object

---
+++ widgets/MenHomeSection/model/men.js
@@ -0,0 +1,300 @@
+import man01 from '../assets/men/man01.jpg';
+import man02 from '../assets/men/man02.jpg';
+import man03 from '../assets/men/man03.jpg';
+import man04 from '../assets/men/man04.jpg';
+import man05 from '../assets/men/man05.jpg';
+import man06 from '../assets/men/man06.jpg';
+
+export const escortMen = [
+  {
+    photo: man01,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'silver',
+    price: '200',
+    verified: 'Not verified'
+  },
+  {
+    photo: man02,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'By photo'
+  },
+  {
+    photo: man03,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'By photo'
+  },
+  {
+    photo: man04,
+    age: 24,
+    name: 'Имя мужчины',
+    rating: '5.0',
+    new: false,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'silver',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man05,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: false,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'silver',
+    price: '200',
+    verified: 'Not verified'
+  },
+  {
+    photo: man06,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'bronze',
+    price: '200',
+    verified: 'By admin'
+  },
+  {
+    photo: man01,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'silver',
+    price: '200',
+    verified: 'By admin'
+  },
+  {
+    photo: man02,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'By admin'
+  },
+  {
+    photo: man03,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man04,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'silver',
+    price: '200',
+    verified: 'By photo'
+  },
+  {
+    photo: man05,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'silver',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man06,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'bronze',
+    price: '200',
+    verified: 'By video'
+  }
+];
+
+export const strippersMen = [
+  {
+    photo: man03,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man04,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'silver',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man05,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'silver',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man06,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'bronze',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man01,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'silver',
+    price: '200',
+    verified: 'Similar'
+  },
+  {
+    photo: man02,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'Not similar'
+  },
+  {
+    photo: man03,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'Not similar'
+  },
+  {
+    photo: man04,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'silver',
+    price: '200',
+    verified: 'Not verified'
+  },
+  {
+    photo: man05,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'silver',
+    price: '200',
+    verified: 'Not verified'
+  },
+  {
+    photo: man06,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: false,
+    background: 'bronze',
+    price: '200',
+    verified: 'Not verified'
+  },
+  {
+    photo: man01,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'silver',
+    price: '200',
+    verified: 'By video'
+  },
+  {
+    photo: man02,
+    name: 'Имя мужчины',
+    age: 24,
+    rating: '5.0',
+    new: true,
+    reviews: '12',
+    manOfTheDay: true,
+    background: 'gold',
+    price: '200',
+    verified: 'By video'
+  }
+];
