7baf2e400da803b647e761e6db9ed26ca7261b69
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-18 16:25:05 +0200

    refactor: reuse home advertisements section

--- entities/Home/ui/HomeAdvertisements/HomeAdvertisements.vue
+++ entities/Home/ui/HomeAdvertisements/HomeAdvertisements.vue
@@ -1,19 +1,21 @@
 <script setup>
 import './home-advertisements.scss';
+
+defineProps({ count: { type: Number, required: true } });
 </script>
 
 <template>
   <div class="home__advertisements">
     <h3 class="home__advertisement-title heading_h5">Реклама</h3>
     <div class="home__advertisement">
+      <div v-for="n in count" :key="n" class="home__advertisement-block">Реклама</div>
+      <!-- <div class="home__advertisement-block">Реклама</div>
       <div class="home__advertisement-block">Реклама</div>
       <div class="home__advertisement-block">Реклама</div>
       <div class="home__advertisement-block">Реклама</div>
       <div class="home__advertisement-block">Реклама</div>
       <div class="home__advertisement-block">Реклама</div>
-      <div class="home__advertisement-block">Реклама</div>
-      <div class="home__advertisement-block">Реклама</div>
-      <div class="home__advertisement-block">Реклама</div>
+      <div class="home__advertisement-block">Реклама</div> -->
     </div>
   </div>
 </template>
