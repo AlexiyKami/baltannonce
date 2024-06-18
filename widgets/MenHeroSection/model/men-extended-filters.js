ee3c760d7f468c7e1e3d68de5dba8a52bf88de7d
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-18 16:06:28 +0200

    feat: add men extended filters object

---
+++ widgets/MenHeroSection/model/men-extended-filters.js
@@ -0,0 +1,494 @@
+export const menExtendedFilters = {
+  meetingPointOptions: [
+    {
+      title: 'Апартаменты Incall',
+      id: 'incall-aa'
+    },
+    {
+      title: 'Отель Incall',
+      id: 'incall-hh'
+    },
+    {
+      title: 'Апартаменты Outcall',
+      id: 'outcall-aa'
+    },
+    {
+      title: 'Отель Outcall',
+      id: 'outcall-hh'
+    },
+    {
+      title: 'Путешествия по приглашению',
+      id: 'invite'
+    }
+  ],
+  sexOptions: [
+    {
+      title: 'Секс классический',
+      id: 'item1'
+    },
+    {
+      title: 'Секс анальный',
+      id: 'item2'
+    },
+    {
+      title: 'Секс групповой',
+      id: 'item3'
+    },
+    {
+      title: 'Услуги семейной паре',
+      id: 'item4'
+    },
+    {
+      title: 'Минет в презервативе',
+      id: 'item5'
+    },
+    {
+      title: 'Минет без резинки',
+      id: 'item6'
+    },
+    {
+      title: 'Горловой Минет',
+      id: 'item7'
+    },
+    {
+      title: 'Куннилингус',
+      id: 'item8'
+    },
+    {
+      title: 'Поцелуи',
+      id: 'item9'
+    },
+    {
+      title: 'Окончание на грудь',
+      id: 'item10'
+    },
+    {
+      title: 'Окончание на лицо',
+      id: 'item11'
+    },
+    {
+      title: 'Окончание в рот',
+      id: 'item12'
+    },
+    {
+      title: 'Сквирт',
+      id: 'item13'
+    },
+    {
+      title: 'Игрушки',
+      id: 'item14'
+    }
+  ],
+  stripteaseOptions: [
+    {
+      title: 'Стриптиз профи',
+      id: 'item15'
+    },
+    {
+      title: 'Стриптиз не профи',
+      id: 'item16'
+    }
+  ],
+  massageOptions: [
+    {
+      title: 'Классический',
+      id: 'item17'
+    },
+    {
+      title: 'Профессиональный',
+      id: 'item18'
+    },
+    {
+      title: 'Урологический',
+      id: 'item19'
+    },
+    {
+      title: 'Эротический',
+      id: 'item20'
+    },
+    {
+      title: 'Лингам',
+      id: 'item21'
+    },
+    {
+      title: 'Карсай',
+      id: 'item22'
+    },
+    {
+      title: 'Массажный стол',
+      id: 'item23'
+    }
+  ],
+  extremeOptions: [
+    {
+      title: 'Страпон',
+      id: 'item24'
+    },
+    {
+      title: 'Анилингус вам',
+      id: 'item25'
+    },
+    {
+      title: 'Анилингус мне',
+      id: 'item26'
+    },
+    {
+      title: 'Золот. дождь выдача',
+      id: 'item27'
+    },
+    {
+      title: 'Золотой дождь прием',
+      id: 'item28'
+    },
+    {
+      title: 'Копро выдача',
+      id: 'item29'
+    },
+    {
+      title: 'Фистинг анальный вам',
+      id: 'item30'
+    },
+    {
+      title: 'Фистинг анальный мне',
+      id: 'item31'
+    },
+    {
+      title: 'Фистинг классический',
+      id: 'item32'
+    },
+    {
+      title: 'Фингеринг',
+      id: 'item33'
+    }
+  ],
+  sadoMasoOptions: [
+    {
+      title: 'Бандаж',
+      id: 'item34'
+    },
+    {
+      title: 'Госпожа',
+      id: 'item35'
+    },
+    {
+      title: 'Игры',
+      id: 'item36'
+    },
+    {
+      title: 'Легкая доминация',
+      id: 'item37'
+    },
+    {
+      title: 'Порка',
+      id: 'item38'
+    },
+    {
+      title: 'Клизма',
+      id: 'item39'
+    },
+    {
+      title: 'Рабыня',
+      id: 'item40'
+    },
+    {
+      title: 'Легкая рабыня',
+      id: 'item41'
+    },
+    {
+      title: 'Фетиш',
+      id: 'item42'
+    },
+    {
+      title: 'Трамплинг',
+      id: 'item43'
+    },
+    {
+      title: 'Боллбастинг',
+      id: 'item44'
+    },
+    {
+      title: 'Феминизация',
+      id: 'item45'
+    }
+  ],
+  differentOptions: [
+    {
+      title: 'Ролевые игры',
+      id: 'item46'
+    },
+    {
+      title: 'Фото/видео съемка',
+      id: 'item47'
+    },
+    {
+      title: 'Эскорт',
+      id: 'item48'
+    },
+    {
+      title: 'Виртуальный секс',
+      id: 'item49'
+    },
+    {
+      title: 'Cекс по телефону',
+      id: 'item50'
+    },
+    {
+      title: 'BFE',
+      id: 'item51'
+    }
+  ],
+  eyeColorOptions: [
+    {
+      title: 'Черный',
+      id: 'black-eyed'
+    },
+    {
+      title: 'Зеленый',
+      id: 'green-eyed'
+    },
+    {
+      title: 'Голубой',
+      id: 'blue-eyed'
+    },
+    {
+      title: 'Серый',
+      id: 'grey-eyed'
+    },
+    {
+      title: 'Желтый',
+      id: 'yellow-eyed'
+    }
+  ],
+  hairColorOptions: [
+    {
+      title: 'Блондин',
+      id: 'blonde2'
+    },
+    {
+      title: 'Шатен',
+      id: 'brown2'
+    },
+    {
+      title: 'Брюнет',
+      id: 'brunette2'
+    },
+    {
+      title: 'Русый',
+      id: 'light-brown2'
+    },
+    {
+      title: 'Рыжий',
+      id: 'red2'
+    }
+  ],
+  hairLengthOptions: [
+    {
+      title: 'Без волос',
+      id: 'hairless'
+    },
+    {
+      title: 'Короткая стрижка',
+      id: 'short-hair'
+    },
+    {
+      title: 'До плеч',
+      id: 'shoulder-hair'
+    },
+    {
+      title: 'До лопаток',
+      id: 'shoulder-blades-hair'
+    },
+    {
+      title: 'До талии',
+      id: 'waist-hair'
+    }
+  ],
+  beardOptions: [
+    {
+      title: 'Полностью выбритый',
+      id: 'beardless'
+    },
+    {
+      title: 'Легкая щетина',
+      id: 'beard-light'
+    },
+    {
+      title: 'Короткая борода',
+      id: 'beard-short'
+    },
+    {
+      title: 'Средняя борода',
+      id: 'beard-mid'
+    },
+    {
+      title: 'Длинная борода',
+      id: 'beard-long'
+    },
+    {
+      title: 'Борода с усами',
+      id: 'beard-mustache'
+    },
+    {
+      title: 'Бакенбарды',
+      id: 'beard-whiskers'
+    }
+  ],
+  languageOptions: [
+    {
+      title: 'Русский',
+      id: 'language-ru'
+    },
+    {
+      title: 'Английский',
+      id: 'language-en'
+    },
+    {
+      title: 'Литовский',
+      id: 'language-lt'
+    },
+    {
+      title: 'Латвийский',
+      id: 'language-lv'
+    },
+    {
+      title: 'Эстонский',
+      id: 'language-ee'
+    },
+    {
+      title: 'Испанский',
+      id: 'language-es'
+    },
+    {
+      title: 'Португальский',
+      id: 'language-pt'
+    },
+    {
+      title: 'Немецкий',
+      id: 'language-de'
+    },
+    {
+      title: 'Польский',
+      id: 'language-pl'
+    },
+    {
+      title: 'Финский',
+      id: 'language-fi'
+    },
+    {
+      title: 'Шведский',
+      id: 'language-sw'
+    },
+    {
+      title: 'Итальянский',
+      id: 'language-it'
+    },
+    {
+      title: 'Французский',
+      id: 'language-fr'
+    }
+  ],
+  intimateHaircutOptions: [
+    {
+      title: 'Полная депиляция',
+      id: 'intimate-haircut-f'
+    },
+    {
+      title: 'Аккуратная стрижка',
+      id: 'intimate-haircut-c'
+    },
+    {
+      title: 'Натуральная',
+      id: 'intimate-haircut-n'
+    }
+  ],
+  tattooPlacesOptions: [
+    {
+      title: 'Лицо',
+      id: 'tattoo-face'
+    },
+    {
+      title: 'Руки',
+      id: 'tattoo-hand'
+    },
+    {
+      title: 'Ноги',
+      id: 'tattoo-leg'
+    },
+    {
+      title: 'Пах',
+      id: 'tattoo-groin'
+    },
+    {
+      title: 'Спина',
+      id: 'tattoo-spine'
+    },
+    {
+      title: 'Бедра',
+      id: 'tattoo-hip'
+    },
+    {
+      title: 'Живот',
+      id: 'tattoo-stomach'
+    },
+    {
+      title: 'Грудь',
+      id: 'tattoo-breast'
+    }
+  ],
+  piercingPlacesOptions: [
+    {
+      title: 'Hoc',
+      id: 'piercing-nose'
+    },
+    {
+      title: 'Брови',
+      id: 'piercing-eyebrows'
+    },
+    {
+      title: 'Язык',
+      id: 'piercing-tongue'
+    },
+    {
+      title: 'Губы',
+      id: 'piercing-lips'
+    },
+    {
+      title: 'Уши',
+      id: 'piercing-ears'
+    },
+    {
+      title: 'Соски',
+      id: 'piercing-nipples'
+    },
+    {
+      title: 'Пупок',
+      id: 'piercing-navel'
+    },
+    {
+      title: 'Пах',
+      id: 'piercing-groin'
+    }
+  ],
+  raceOptions: [
+    {
+      title: 'Белая',
+      id: 'race-white'
+    },
+    {
+      title: 'Латинская',
+      id: 'race-latin'
+    },
+    {
+      title: 'Черная',
+      id: 'race-negro'
+    },
+    {
+      title: 'Азиатская',
+      id: 'race-asian'
+    },
+    {
+      title: 'Экзотическая',
+      id: 'race-exotic'
+    }
+  ]
+};
