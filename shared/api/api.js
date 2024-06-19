005bb2321804395f2451fafd51857f92ee1afaa7
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-19 19:06:11 +0200

    feat: create register advertiser api request

--- shared/api/api.js
+++ shared/api/api.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 const baseURL = 'http://91.208.197.79:8000';
 
 export default class Loader {
-  static async register(user) {
+  static async registerUser(user) {
     axios
       .post(`${baseURL}/auth/customer/registration/`, user)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  static async registerAdvertiser(advertiser) {
+    axios
+      .post(`${baseURL}/auth/model/registration/`, advertiser)
       .then(function (response) {
         console.log(response);
       })
