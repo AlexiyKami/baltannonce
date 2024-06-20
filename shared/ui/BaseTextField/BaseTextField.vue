e2855d9487d899b036c5f6e75b0fd7e18c7f79b8
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-20 14:14:32 +0200

    feat: create base text field component to reuse

---
+++ shared/ui/BaseTextField/BaseTextField.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  placeholder: { type: String, required: true },
+  type: { type: String, required: true },
+  value: { type: String, required: true }
+});
+
+const emits = defineEmits(['setValue', 'setFocus']);
+
+const handleInput = (e) => {
+  emits('setValue', e.target.value);
+};
+
+const handleFocus = (value) => {
+  emits('setFocus', value);
+};
+</script>
+
+<template>
+  <div class="register__input-container">
+    <input
+      class="register__input"
+      :type="type"
+      :value="value"
+      :placeholder="placeholder"
+      @input="handleInput"
+      @focus="handleFocus(true)"
+      @blur="handleFocus(false)"
+    >
+    <slot />
+  </div>
+</template>
