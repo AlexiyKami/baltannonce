d83f878894bac0d0176a016990eb568d3ad77b04
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-20 14:19:02 +0200

    refactor: put password field in separate component to reuse

---
+++ entities/Password/index.js
@@ -0,0 +1,5 @@
+import PasswordField from './ui/PasswordField/PasswordField.vue';
+import PasswordConfirmationField from './ui/PasswordConfirmationField/PasswordConfirmationField.vue';
+
+export { PasswordField };
+export { PasswordConfirmationField };
