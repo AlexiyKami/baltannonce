837d0927276b6189e2a040fb136e54367ba4e080
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-20 09:15:53 +0200

    feat: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/app/assets/scss/main.scss'],
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@pinia/nuxt']
 });
