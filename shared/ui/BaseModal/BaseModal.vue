490787402f7323eda07f711f0835e2b2831c8f8e
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-07-01 11:48:45 +0200

    feat: add base modal component

---
+++ shared/ui/BaseModal/BaseModal.vue
@@ -0,0 +1,12 @@
+<script setup>
+import './base-modal.scss';
+
+defineEmits(['closeModal']);
+</script>
+
+<template>
+  <div class="modal">
+    <div class="modal__overlay" @click="$emit('closeModal')" />
+    <slot />
+  </div>
+</template>
